bb40feeae1edea8ee358016602379aa62a0355dc
Prashant mandawdhare <mprashant@gmail.com>
2015-01-15 15:57:56 -0800

    first check
    first check

---
+++ NorthwindDemoApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NorthwindDemoApp.Startup))]
+namespace NorthwindDemoApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
