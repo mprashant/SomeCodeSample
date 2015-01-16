c74fbccfa330c9ab6c323a76f6d33ec1e4b0b77b
Prashant mandawdhare <mprashant@gmail.com>
2015-01-15 22:34:12 -0800

    add initial code

---
+++ NorthwindDemoApp/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace NorthwindDemoApp.Controllers
+{
+	/// <summary>
+	/// Description of Employee.
+	/// </summary>
+	public class EmployeeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+		
+		public ActionResult Create()
+		{
+			return View();
+		}
+		
+		[HttpPost]
+		public ActionResult Create(FormCollection values)
+		{
+			return View();
+		}
+		
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+		
+		[HttpPost]
+		public ActionResult Edit(int id, FormCollection values)
+		{
+			return View();
+		}
+		
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+	}
+}
