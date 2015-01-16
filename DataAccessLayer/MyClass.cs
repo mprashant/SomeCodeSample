8f8bbca7355c437727276532033c95ae2e73f05f
Prashant mandawdhare <mprashant@gmail.com>
2015-01-15 22:00:07 -0800

    added initial structure

---
+++ DataAccessLayer/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: prash
+ * Date: 1/15/2015
+ * Time: 9:49 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace DataAccessLayer
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
---
+++ RepositoryLayer/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: prash
+ * Date: 1/15/2015
+ * Time: 9:45 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace RepositoryLayer
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
